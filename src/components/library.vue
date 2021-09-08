a15a1a2300bfa5d753589c0dc5f6727addbe768c
Alexandr <sasha-is-permi@mail.ru>
2021-09-08 14:09:56 +0500

    1

---
+++ src/components/library.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+   <h2> {{name}}</h2>
+   <p><span v-html="description"></span></p>
+   <p>Адрес: {{ adress}}</p>
+   <p>сайт: <a :href="websait" target="_new">{{ websait }}</a></p>
+   <p>email: <a :href="'mailto:'+email"> {{ email }}</a>  </p>
+   <p>телефон: <a :href="'tel:'+tel"> {{ tel }}</a>  </p>
+    <p> <img class="img" :src="img"/> </p>
+    <div>
+     <router-link class="red" to="/List">  Вернуться к списку библиотек   </router-link> 
+    </div> 
+ </div>   
+</template>
+
+<script>
+export default{
+ data(){   
+ return{
+ name:     this.$route.params.library.data.general.name || " " ,
+ description: this.$route.params.library.data.general.description || " ",
+ adress:  this.$route.params.library.data.general.address.street || " ", 
+ websait:  this.$route.params.library.data.general.contacts.website || " " ,
+ library: this.$route.params.library || " ",
+ email: this.$route.params.library.data.general.contacts.email || " ",
+ tel: this.$route.params.library.data.general.contacts.phones[0].value || " " ,
+ img: this.$route.params.library.data.general.image.url || " " 
+ }
+ }
+}
+</script>
+
+<style scoped>
+
+.red{
+    color:#c00;
+}
+
+.img{
+    width:300px;
+}
+
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="libraryes">
+        <p> Фильтр: </p>
+        <input type="text" v-model="librarySearch"/>
+        <p class="library"  v-for="library in libraryes" :key="library._id">
+        <router-link 
+        :to="{name:'Library' ,params:{library:library}}"    
+        >   {{library.data.general.name}}  </router-link>
+        </p>   
+    </div>
+</template>
+
+
+<script>
+   export default{
+       data() {
+         return{
+           librarySearch:""    
+         }
+       },
+           
+              computed: {           
+              libraryes() {
+                  let libraryes2 = this.$store.getters.libraryes;
+                  console.log("libraryes", libraryes2)
+                
+                  libraryes2 = libraryes2.filter(item => {
+                  return item.data.general.name.toUpperCase().indexOf(this.librarySearch.toUpperCase())!==-1;
+                    })
+
+                  console.log("libraryes2", libraryes2)
+                    return libraryes2;
+            }
+            }
+                
+                }
+
+                
+            
+
+ 
+
+</script>
+
+<style scoped>
+
+</style>
